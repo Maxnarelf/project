bdec60c9f8dd46a8ceae7ec8b4a3bdc9a5770aa6
Sergey <maxnarelf@gmail.com>
2021-06-09 23:55:48 +0300

    учу циклы

--- JS/script.js
+++ JS/script.js
@@ -10,12 +10,59 @@ const personalMovieDb = {
     privat: false   
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его ?', ''),
-      c = prompt('Один из последних просмотренных фильмов', ''),
-      d = prompt('На сколько оцените его ?', '');
+for (let i = 0; i < 2; i++) {
+   const a = prompt('Один из последних просмотренных фильмов', ''),
+         b = prompt('На сколько оцените его ?', '');
+         if (a && b && a.length < 50) {
+        personalMovieDb.movies[a] = b;
+        console.log('done');  
+    }else{
+        console.log('error');
+        i--;
+    }
+  }
 
-personalMovieDb.movies[a] = b;
-personalMovieDb.movies[c] = d;
+ 
+// let i = 0;
+// while (i < 2){
+//     i++;
+//     const a = prompt('Один из последних просмотренных фильмов', ''),
+//           b = prompt('На сколько оцените его ?', '');     
 
-console.log(personalMovieDb);
+// if (a != '' && b != null && a != '' && b != '' && a.length < 50){
+//   personalMovieDb.movies[a] = b;
+//   console.log('done');
+//   } else {
+//   console.log('error');
+//   i--;
+// } 
+   
+// } 
+
+// let i = 0;
+// do {
+//     const a = prompt('Один из последних просмотренных фильмов', ''),
+//           b = prompt('На сколько оцените его ?', '');  
+         
+//           if (a != '' && b != null && a != '' && b != '' && a.length < 50){
+//             personalMovieDb.movies[a] = b;
+//             console.log('done');
+//             i++;
+//             } else {
+//             console.log('error');
+//             i--;
+//          }    
+//     }
+// while (i < 2); 
+
+if (personalMovieDb.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+}else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30 ) {
+    console.log('Вы классический зритель'); 
+}else if (personalMovieDb.count >= 30) {
+    console.log('Вы киноман');
+}else {
+    console.log('ошибка');
+}
+
+console.log(personalMovieDb); 
